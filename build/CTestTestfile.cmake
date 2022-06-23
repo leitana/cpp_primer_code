7b2ddc0e29a728a4c194f655186ed5160d383782
linxiao <1130037266@qq.com>
2022-06-23 09:37:12 +0800

    update

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/CppWorkSpace
-# Build directory: D:/CppWorkSpace/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
