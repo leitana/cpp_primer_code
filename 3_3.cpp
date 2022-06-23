fab0d8537af9bcd302d1ee88409326a1f0bf7a05
linxiao <1130037266@qq.com>
2022-06-23 09:40:38 +0800

    删除build 增加忽略文件

--- 3_3.cpp
+++ 3_3.cpp
@@ -1,7 +1,12 @@
 #include <vector>
+#include "Sales_items.h"
+using std::string;
 using std::vector;
 
 int main()
 {
+    vector<int> ivec;
+    vector<Sales_item> sales_item;
+    vector<vector<string>> file;
     return 0;
 }
