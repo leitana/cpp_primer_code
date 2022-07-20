c13a9142f52abaf031fa35800c8ab97b1e61022c
linxiao <1130037266@qq.com>
2022-07-20 16:52:47 +0800

    Person.cpp

--- 7/Person.cpp
+++ 7/Person.cpp
@@ -4,10 +4,23 @@ using namespace std;
 
 struct Person
 {
+    friend istream &read(istream &is, Person &person);
+    friend ostream &print(ostream &os, const Person &person);
+
+private:
     string name;
     string address;
 
 public:
+    Person() = default;
+    Person(const string sname, const string saddr) : name(sname), address(saddr) {}
+    // Person(const string sname, const string saddr)
+    // {
+    //     name = sname;
+    //     address = saddr;
+    // }
+    Person(istream &is) {}
+
     string const &get_name() const { return name; }
     string const &get_addr() const { return address; }
 };
